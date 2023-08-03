faaae7d4e5c8db19c41040e2fa6764bdcf0f1c23
Jarad Overby <joverby@css.edu>
2023-08-03 15:55:24 -0500

    Comments that lay out the problems that need to be solved for program

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+console.log("Hello World");
+
+/* 
+Rock Paper Scissors Game:
+Logic -> Paper > Rock : Scissors > Paper : Rock > Scissors
+
+1: Get user input from prompt
+
+2: Create function to generate random selection for computer
+    2.a: Return randomized value
+
+3: Create function to compare user input to computer selection (from function)
+    3.a: return true or string if user beats computer
+    3.b: return true or string if computer beats user
+    3.c: return string stating game was a draw
+
+4: Create score variables
+    4.a: increment winner score
+
+5: Bonus -> Play game within loop
+    5.a: Get user input on how many iterations of game they want to play
+    5.b: Generate 'game over' and 'winner/ loser' text when game is done and depending on user score compared to computers
+*/
